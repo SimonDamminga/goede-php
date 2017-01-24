f86458c7ba88970873c7024bb3beb336140f859b
Simon Damminga <99039973@mydavinci.nl>
2017-01-24 15:40:21 +0100

    bug fix: hij loopt nu niet meer vast

--- index.php
+++ index.php
@@ -5,38 +5,42 @@
 	<link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<title></title>
+	<title>Goede</title>
 </head>
 <body id="background">
 <script type="text/javascript">
-	setInterval(function(){
-		$("#reload").load(location.href + "#reload");
-	}, 10000)
+	var timeout = setTimeout(reload, 1000);
+	function reload(){
+		$("#reload").load('index.php #reload', function(){
+			$(this).unwrap();
+			timeout = setTimeout(reload, 1000);
+		});
+	}
 </script>
 <div id="reload">
 	<?php 
 		date_default_timezone_set('Europe/Amsterdam');
-		$time = date("h:i");
+		$time = date("H:i");
 		$text = "";
 		echo "<br>";
-		if($time >= "06:00" and $time <= "12:00"){
+		if($time >= "06:00" and $time < "12:00"){
 			$text = "Goede morgen!";
 			echo "<h1>$text</h1><p>Het is nu $time uur</p>";
 			echo "<style>#background{background-image: url(backgrounds/morning.png); background-attachment: fixed; background-size: cover;}</style>";
-		}elseif($time >= "12:00" and $time <= "18:00"){
+		}elseif($time >= "12:00" and $time < "18:00"){
 			$text = "Goede middag!";
 			echo "<h1>$text</h1><p>Het is nu $time uur</p>";
 			echo "<style>#background{background-image: url(backgrounds/afternoon.png);}</style>";
-		}elseif ($time >= "18:00" and $time <= "00:00") {
+		}elseif ($time >= "18:00" and $time < "00:00") {
 			$text = "Goede avond!";
 			echo "<h1>$text</h1><p>Het is nu $time uur</p>";
 			echo "<style>#background{background-image: url(backgrounds/evening.png);}</style>";
-		}elseif ($time >= "00:00" and $time <= "06:00") {
+		}elseif ($time >= "00:00" and $time < "06:00") {
 			$text = "Slaap lekker!";
 			echo "<h1>$text</h1><p>Het is nu $time uur</p>";
 			echo "<style>#background{background-image: url(backgrounds/night.png);}</style>";
 		}
- 	?>
+	 ?>
  </div>
 </body>
 </html>
